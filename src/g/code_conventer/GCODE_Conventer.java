828c200c2a058d5f5295a5bc8829479b8c2c11ac
Karol Robak <karolloo916@gmail.com>
2022-06-25 23:11:26 +0200

    little cleanup

--- src/g/code_conventer/GCODE_Conventer.java
+++ src/g/code_conventer/GCODE_Conventer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package g.code_conventer;
 
 
@@ -16,19 +11,17 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/*import jssc.SerialPort;
-import jssc.SerialPortList;
-import jssc.SerialPortException;*/
 /**
- *
- * @author Karol
+ *Set of function to transform cartesian to scara and send to arduino
+ * 
+ * @author Karol Robak
  */
 public class GCODE_Conventer {
 
+    private static boolean started=false;
     private static final String src="C:\\Users\\Karol\\Desktop\\xd2.txt";
     private static final String out="C:\\Users\\Karol\\Desktop\\test2.txt";
     
@@ -39,25 +32,17 @@ public class GCODE_Conventer {
 
     private static final long Lr=100; //long arm length 
     private static final long Sr=100;//short arm length
-    private static final int DEGREE_BIG_ARM=130;
-    private static final int DEGREE_SMALL_ARM=155;
     private static final long R=200;
     private static final long r=40;
 
-    
-    //private static final int MOTOR_STEPS_PRER_ROTATION=360;    //1.8 degree
-    private static final int MOTOR_STEPS_PRER_ROTATION=200;    //1.8 degree
+    private static final int MOTOR_STEPS_PRER_ROTATION=200;    //200 steps in 1 step mode, 400 in 1/2 step mode...
 
     private static final double ARM_LONG_STEPS_PER_ROTATION=/*MOTOR_STEPS_PRER_ROTATION;*/35D/20D;              //1.75
     private static final double ARM_SHORT_DEGREES_BY_ROTATION=/*MOTOR_STEPS_PRER_ROTATION;*/(116D/25D);         //116x30x25
     private static final double ARM_SHORT_ADDITIONAL_ROTATION=/*MOTOR_STEPS_PRER_ROTATION;*/(30D/25D);         //116x30x25
-    /*
-    private static final double ARM_LONG_STEPS_PER_ROTATION=20D*MOTOR_STEPS_PRER_ROTATION/35D/360;              
-    private static final double ARM_SHORT_DEGREES_BY_ROTATION=25D*MOTOR_STEPS_PRER_ROTATION/116D/360;
-    */
-    
+
     private static final int RAPID_SPEED=300;
-   // private int speed=0;
+
     
     private static enum REL_STATE{
         G90,    //absolute
@@ -67,47 +52,21 @@ public class GCODE_Conventer {
     private static boolean rapid=false;
     private static double speedrate=1.0;
     private static long [] position = {/*-R*/0,0,0};
-    private static double [] angles = {/*DEGREE_BIG_ARM*//*-9*/0,0/*DEGREE_SMALL_ARM/2*/};
+    private static double [] angles = {/*DEGREE_BIG_ARM*//*-9*/0,-180/*DEGREE_SMALL_ARM/2*/};
     private static Object lock=new Object();
     private static Thread t;
     
-   // private static  SerialPort serialPort;
-    
-    public static void test(){
-    
-        int xSteps=111;
-        int ySteps=433;   
-        int iterator=0;   
-        int licz=0;
-        char biggerChar='Y';
-        double scale=3.9;
-        while(xSteps>0||ySteps>0){
-           ++iterator;
-           if(scale!=1){
-                if(((int)Math.floor(iterator/scale))>licz){
-                    ++licz;
-                    if(biggerChar=='X'&&ySteps>0)
-                        --ySteps;
-                    else if(xSteps>0)
-                        --xSteps;
-                    }
-                if(biggerChar=='X'&&xSteps>0)
-                    --xSteps;
-                else if(ySteps>0)
-                    --ySteps;
-           }
-            System.out.println("TEST x: "+xSteps+"  y: "+ySteps);
-        }
-    
-    }
-    
-    
     public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException {
      
-
+    makeGCodeFile();
         System.out.println("spr "+ARM_LONG_STEPS_PER_ROTATION+" "+ARM_SHORT_DEGREES_BY_ROTATION);
-    sendGcode();
+   // sendGcode();
     }
+    
+    /**
+     * @throws InterruptedException
+     * @throws IOException 
+     */
     private static void sendGcode()throws  InterruptedException, IOException{
 
             
@@ -150,18 +109,14 @@ public class GCODE_Conventer {
                             public void run() {
                                try {
                              String line;
-                             String got;
                                int i=0;  
                            
                             while((line=br.readLine())!=null){
-                                // serialPort.writeString(line);
-                                //   uno.sendMessage(line); 
-                               // is.skip(1000);
+                             
                                 pw.write(line);
                                 pw.flush();  
                                 byte[] buffer = new byte[1024];
                                  bins.read(buffer);
-                                 got=new String(buffer);
                                 System.out.println(""+(++i)+ " "+line);
                                 sleep(1000);
                           
@@ -197,7 +152,10 @@ public class GCODE_Conventer {
          
     }
  
-    
+    /**
+     * Do what name says
+     * @throws IOException 
+     */
     private static void makeGCodeFile() throws IOException{
      
         File fin=new File(src);
@@ -253,6 +211,7 @@ public class GCODE_Conventer {
     }
     
     /**
+     * Takes raw code and change positioning
      * @param comand comand, every row is option of comand
      * @return translated to scara comand line
      */
@@ -283,7 +242,11 @@ public class GCODE_Conventer {
                }
         return ret;
     }
-
+    /**
+     * Reads params from array and sends to Transition
+     * @param code array of comands
+     * @return calculated scara 
+     */
     private static String calculate(String code[]){
          String ret="";
          double x=-1;
@@ -324,12 +287,9 @@ public class GCODE_Conventer {
         return "";
     }
     /**
-     * 
-     * @param xMove no move if -1
-     * @param yMove no move if -1
+     * All magic happens here
      * @return 
      */
-    
     private static String Transition(double xMove,double yMove,double zMove,double speed){
         String comand="";
         double xm=xMove;
@@ -340,9 +300,9 @@ public class GCODE_Conventer {
             
         }
      
-        if(xMove==-1&&xm==-1)
+        if(xMove==0&&xm==0)
             xm=position[0];
-        if(yMove==-1&&ym==-1)
+        if(yMove==0&&ym==0)
             ym=position[1];
         double newRaius=Math.hypot(xm, ym);
         if(newRaius>R)
@@ -350,35 +310,52 @@ public class GCODE_Conventer {
         double alpha;
         double beta;
      
-        double gamma=Math.atan2(ym, xm);//absolute degree
-        beta=Math.acos(((xm*xm)+(ym*ym)-(Lr*Lr)-(Sr*Sr)/*(Lr*Lr)+(Sr*Sr)-(newRaius*newRaius)*/)/(-2*Lr*Sr));//between  arms 
-        alpha=Math.asin((Lr*Math.sin(/*gamma-*/Math.PI-beta))/newRaius);   //between first arm and R
-         System.out.println(/*"lr "+Lr+*/"beta "+beta+" newRadius "+newRaius);
-
+        double gamma=Math.atan2(ym, xm);//absolute degree angle to R
+        beta=Math.acos(((xm*xm)+(ym*ym)-(Lr*Lr)-(Sr*Sr))/(-2*Lr*Sr));//between  arms 
+        alpha=Math.asin((Lr*Math.sin(Math.PI-beta))/newRaius);   //between first arm and R
+     
         double alphaAdd=Math.toDegrees(gamma+alpha);
      
         double betaAdd=Math.toDegrees(beta); 
-      
-       System.out.println("ladd "+alphaAdd+" "+ Math.toDegrees(alpha)+ " badd "+betaAdd+" "+Math.toDegrees(beta)  +" gamma "+Math.toDegrees(gamma));
-       
-       double betaChange=(betaAdd>0)?180D-betaAdd:-180D-betaAdd;
-     
-        double alphaNow=((alphaAdd-angles[0])*ARM_LONG_STEPS_PER_ROTATION)*MOTOR_STEPS_PRER_ROTATION/360;
         
-        double betaNow=((betaChange-angles[1])*ARM_SHORT_DEGREES_BY_ROTATION)*MOTOR_STEPS_PRER_ROTATION/360+((alphaAdd-angles[0])*ARM_SHORT_ADDITIONAL_ROTATION)*MOTOR_STEPS_PRER_ROTATION/360;
-
-         System.out.println("l2 "+alphaAdd+ " b2 "+betaAdd+" g2 "+Math.toDegrees(gamma)+"\n");
-           System.out.println("l2 "+alphaNow+ " b2 "+betaNow+" g2 "+Math.toDegrees(gamma)+"\n");
+           System.out.println("aA "+alphaAdd+" ang "+angles[0]);
        
-        position[0]=(long) xm;
+       double betaChange=
+               180-betaAdd;                                             ///what if betaAdd is less than 0
+             
+        if(Double.isNaN(alphaAdd))
+             alphaAdd=0;
+         if(Double.isNaN(betaChange))
+             betaChange=0;
+
+        System.out.println("NewADegree "+(alphaAdd-angles[0])); 
+         
+        double alphaNow=((alphaAdd-angles[0])*ARM_LONG_STEPS_PER_ROTATION)*MOTOR_STEPS_PRER_ROTATION/360;
+     
+         System.out.println("NewASteps "+alphaNow); 
+    
+         
+        double betaNow=((betaChange-angles[1])*ARM_SHORT_DEGREES_BY_ROTATION)*MOTOR_STEPS_PRER_ROTATION/360+((alphaAdd-angles[0])*ARM_SHORT_ADDITIONAL_ROTATION)*MOTOR_STEPS_PRER_ROTATION/360;//drugi człon do poprawy
+       
+         position[0]=(long) xm;
         position[1]=(long) ym;
         
+        if((betaNow<0&&(betaChange-angles[1])>0||(betaNow>0&&(betaChange-angles[1])<0))){
+            betaNow*=-1;
+        
+        }
+           
         comand+=""+L+((long)alphaNow)+
                 " "+S+((long)betaNow);
-         angles[0]=alphaAdd;
-         angles[1]=betaAdd;
-
-        if(zMove!=-1){
+       
+        if((!Double.isNaN(alphaAdd))&&alphaAdd!=0)
+             angles[0]=angles[0]+(alphaAdd-angles[0]);
+        if((!Double.isNaN(betaChange))&&betaChange!=0)
+            angles[1]=  //betaAdd-angles[1];
+            angles[1]+(betaChange-angles[1]);
+        
+      
+        if(zMove!=0){
             comand+=" Z"+(zMove*MOTOR_STEPS_PRER_ROTATION);
         }
         if(rapid)
